0956773fc4be482be06beac3c14521a5e3af0fc2
weichichou <weichi.cc@gmail.com>
2019-11-13 09:59:41 +0100

    render ad list

--- src/components/AdsList.js
+++ src/components/AdsList.js
@@ -4,11 +4,13 @@ import {Link} from 'react-router-dom'
 export default class AdsList extends React.Component {
     render(){
         const list = this.props.ads.map(ad => {
-            return <h5 key={ad.id}>
+            return <div key={ad.id}>
                    <Link to={`/ads/${ad.id}`} >
-                     {ad.title}
+                     <h3>{ad.title}</h3>
+                     <img src={ad.pictureUrl} alt={ad.title} width='150px'/>
                    </Link>
-                  </h5>
+                   <h4>Price: {ad.price} €</h4>
+                  </div>
         })
 
         return(
